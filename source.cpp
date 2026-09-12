c93c222e2b0ff452927ec925edd8fa786ec22345
ivanglavatchi20 <ivanglavat@gmail.com>
2026-09-12 21:29:22 +0300

    add file

---
+++ source.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+class Transport 
+{
+
+private:
+
+	std::string name;
+	float speed;
+	unsigned short enginecapacity;
+	unsigned short wheels;
+	unsigned short seats;
+
+public:
+
+	Transport(std::string name, float speed, unsigned short enginecapacity, unsigned short wheels, unsigned short seats) : name(name), speed(speed), enginecapacity(enginecapacity), wheels(wheels), seats(seats) 
+	{
+
+
+
+	}
+	~Transport() {}
+
+};
+
+class Car 
+{
+
+
+
+};
+
+class Motorcycle 
+{
+
+
+
+};
+
+class OtherTransport
+{
+
+
+
+};
+
+
+class Car : public Transport
+{
+
+
+
+};
+
+class Motorcycle : public Transport
+{
+
+
+
+};
+
+class OtherTransport : public Transport
+{
+
+
+
+};
+
+int main()
+{
+	
+	std::cout << "Nasledovanie!!" << std::endl;
+	return 0;
+
+}
