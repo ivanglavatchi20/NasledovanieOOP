f6b0fbab921f6650db73320b037ea7238d8df7c4
ivanglavatchi20 <ivanglavat@gmail.com>
2026-09-16 10:06:23 +0300

    add neurocode

--- source.cpp
+++ source.cpp
@@ -1,93 +1,98 @@
 ﻿#include <iostream>
 #include <locale>
+#include <string>
 
-
-
-class Transport 
+class Transport
 {
-
-private:
-
-	std::string name;
-	float speed;
-	unsigned short enginecapacity;
-	unsigned short wheels;
-	unsigned short seats;
-	unsigned short weight;
+protected:
+    std::string name;
+    float speed;
+    unsigned short enginecapacity;
+    unsigned short wheels;
+    unsigned short seats;
+    unsigned short weight;
 
 public:
+    Transport(std::string name, float speed, unsigned short enginecapacity,
+        unsigned short wheels, unsigned short seats, unsigned short weight)
+        : name(name), speed(speed), enginecapacity(enginecapacity),
+        wheels(wheels), seats(seats), weight(weight)
+    {
+        std::cout << "Transport created: " << name << std::endl;
+    }
 
-	Transport(std::string name, float speed, unsigned short enginecapacity, unsigned short wheels, unsigned short seats, unsigned short weight) : name(name), speed(speed), enginecapacity(enginecapacity), wheels(wheels), seats(seats), weight(weight)
-	{
-		std::cout << "Transport created: " << name << std::endl;
-	}
-	~Transport() {}
-	
-	void move(std::string name, float speed, unsigned short enginecapacity, unsigned short wheels, unsigned short seats, unsigned short weight)
-	{
-		std::cout << "Эта херня(" << name << ") с " << seats << " сидениями движется со скоростью " << speed << " км/ч. При объеме двигателя " << enginecapacity << " см³" << "весом " << weight << "кг" << " и количеством сидений " << seats << std::endl;
-		std::cout << "Это чудо, как эта херня вообще поехала. Ведь утром она не заводилась/не ехала" << std::endl;
-	}
-	void moveElectric(std::string name, float speed, unsigned short wheels, unsigned short weight)
-	{
-		std::cout << "Эта херня(" << name << ") с " << seats << " сидениями движется со скоростью " << speed << " км/ч. Весом " << weight << "кг" << std::endl;
-		std::cout << "Это чудо, как эта херня вообще поехала. Ведь утром она не заводилась/не ехала" << std::endl;
-	}
+    virtual ~Transport() {}
 
+    virtual void move() const
+    {
+        std::cout << "Транспорт (" << name << ") с " << seats
+            << " сидениями движется со скоростью " << speed << " км/ч. "
+            << "Объем двигателя " << enginecapacity << " см3, вес "
+            << weight << " кг, сидений: " << seats << std::endl;
+    }
 };
+
 class Car : public Transport
 {
-private:
-	std::string name;
-	float speed;
-	unsigned short enginecapacity;
-	unsigned short wheels;
-	unsigned short seats;
-	unsigned short weight;
 public:
-	Car(std::string name, float speed, unsigned short enginecapacity, unsigned short wheels, unsigned short seats, unsigned short weight) : Transport(name, speed, enginecapacity, wheels, seats, weight) {}
+    Car(std::string name, float speed, unsigned short enginecapacity,
+        unsigned short wheels, unsigned short seats, unsigned short weight)
+        : Transport(name, speed, enginecapacity, wheels, seats, weight) {
+    }
 
 };
 
 class Motorcycle : public Transport
 {
-private:
-	std::string name;
-	float speed;
-	unsigned short enginecapacity;
-	unsigned short wheels;
-	unsigned short seats;
-	unsigned short weight;
 public:
-	Motorcycle(std::string name, float speed, unsigned short enginecapacity, unsigned short wheels, unsigned short seats, unsigned short weight) : Transport(name, speed, enginecapacity, wheels, seats, weight) {}
+    Motorcycle(std::string name, float speed, unsigned short enginecapacity,
+        unsigned short wheels, unsigned short seats, unsigned short weight)
+        : Transport(name, speed, enginecapacity, wheels, seats, weight) {
+    }
 
-	
+    void move() const override
+    {
+        std::cout << "Мотоцикл (" << name << ") мчится со скоростью "
+            << speed << " км/ч, объем двигателя " << enginecapacity
+            << " см3, вес " << weight << " кг" << std::endl;
+    }
 };
 
 class OtherTransport : public Transport
 {
-private:
-	std::string name;
-	float speed;
-	unsigned short wheels;
-	unsigned short weight;
 public:
-	OtherTransport(std::string name, float speed, unsigned short wheels, unsigned short weight) : Transport(name, speed, 0, wheels, 1, weight) {}
+    OtherTransport(std::string name, float speed, unsigned short wheels, unsigned short weight)
+        : Transport(name, speed, 0, wheels, 1, weight) {
+    }
 
+    void move() const override
+    {
+        std::cout << "Транспорт (" << name << ") без двигателя едет со скоростью "
+            << speed << " км/ч, вес " << weight << " кг" << std::endl;
+    }
 };
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	Transport transport("Mustang", 120, 2000, 4, 5, 1500);
-	Car car("Toyota_Yaris", 150, 2500, 4, 5, 1200);
-	Motorcycle motorcycle("Yamaha", 250, 1000, 2, 2, 150);
-	OtherTransport other("Bicycle", 30  , 2, 7);
+    setlocale(LC_ALL, "Russian");
 
-	transport.move("Mustang", 120, 2000, 4, 5, 1500);
-	car.move("Toyota_Yaris", 150, 2500, 4, 5, 1200);
-	motorcycle.move("Yamaha", 250, 1000, 2, 2, 150);
-	other.moveElectric("Bicycle", 30, 2, 7);
+    Transport transport("Mustang", 120, 2000, 4, 5, 1500);
+    Car car("Toyota_Yaris", 150, 2500, 4, 5, 1200);
+    Motorcycle motorcycle("Yamaha", 250, 1000, 2, 2, 150);
+    OtherTransport other("Bicycle", 30, 2, 7);
 
-	return 0;
+
+    transport.move();
+    car.move();
+    motorcycle.move();
+    other.move();
+
+    std::cout << "\n--- Полиморфный вызов через указатели на Transport ---\n";
+    Transport* fleet[] = { &transport, &car, &motorcycle, &other };
+    for (Transport* t : fleet)
+    {
+        t->move();
+    }
+
+    return 0;
 }
